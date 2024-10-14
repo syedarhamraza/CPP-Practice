9be6832e21c1b042de0b643b2a0c7d3ff5b29c9e
arhammastery <arhamraza399@gmail.com>
2024-10-14 20:26:58 +0500

    add multi array

---
+++ multidimensional-array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string letters[2][4] = {
+        {"A", "B", "C", "D"},
+        {"E", "F", "G", "H"}};
+
+    cout << letters[1][2];
+    return 0;
+}
